37bab2055ebb27147a8212790ccc62c2388a0bf4
ghost-aramic <owinoojala@gmail.com>
2023-11-13 10:13:56 +0300

    feat: Connect to DB

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,65 @@
-import express, { Request, Response } from "express"
+import express, { Application, Request, Response } from "express"
 import jwt from "jsonwebtoken"
 import 'dotenv/config'
-const app = express()
+import { HydratedUserDoc, User } from "./user.model"
+import mongoose from "mongoose"
+const app: Application = express()
 
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
+const MONGODB_URI = process.env.MONGODB_URI
 
-app.get('/', (req: Request, res: Response) =>{
-    res.json({name: "Welcome Home"})
-})
-app.post('/sign-up', (req: Request, res: Response) =>{
-    const secretKey = process.env.TOKEN_SECRET
-    if(secretKey){
-        const {full_name, username, email, password } = req.body
-        const token = jwt.sign(
-            {username, email}, 
-            secretKey,
-            {
-                issuer: 'localhost',
-                expiresIn: '1m',
-            }
-        )
-        res.json({ token })
-    } else{
-        res.status(500).json("Internal server error")
-    }
-})
+if(MONGODB_URI){
+    mongoose.connect(MONGODB_URI).then(result =>{
+        console.log("Connected to DB")
 
-app.post('/login', (req: Request, res: Response) =>{
-    const secretKey = process.env.TOKEN_SECRET
+        runserver(app)
 
-    if(secretKey){
-        const token = req.headers['authorization']?.split(' ')[1]
-        if(token){
-            const user: string | jwt.JwtPayload = jwt.verify(token, secretKey)
-            res.json({ user })
-        } else {
-            res.status(401).json("Unauthorized")
+    }).catch(error =>{
+        console.log(`Connection to DB failed: ${error.message}`)
+    })
+}
+
+function runserver(app: Application){
+    app.get('/', (req: Request, res: Response) =>{
+        res.json({name: "Welcome Home"})
+    })
+    app.post('/sign-up', async(req: Request, res: Response, next) =>{
+    
+        try {
+            const {full_name, username, email, password } = req.body
+            const user: HydratedUserDoc = new User({
+                full_name, password, email, username
+            })
+        
+            const savedUser = await user.save()
+            console.log(savedUser)
+            
+            res.status(201).json({ 
+                userId: savedUser.id, 
+                username: savedUser.username,
+                email: savedUser.email 
+            })
+            
+        } catch (error) {
+            next(error)   
         }
-    } else {
-        res.status(500).json("Internal server error")
-    }  
-})
-app.listen(4000)
+    })
+    
+    app.post('/login', (req: Request, res: Response) =>{
+        const secretKey = process.env.TOKEN_SECRET
+    
+        if(secretKey){
+            const token = req.headers['authorization']?.split(' ')[1]
+            if(token){
+                const user: string | jwt.JwtPayload = jwt.verify(token, secretKey)
+                res.json({ user })
+            } else {
+                res.status(401).json("Unauthorized")
+            }
+        } else {
+            res.status(500).json("Internal server error")
+        }  
+    })
+    app.listen(4000)
+}
